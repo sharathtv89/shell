7e9a4d581839f528fadb5a17f95f4a3c97f08b4c
sharathtv89 <sharathtv89@mail.com>
2025-01-01 20:24:22 +0530

    Handled Inavlid Commands

--- src/main.cs
+++ src/main.cs
@@ -1,7 +1,16 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Collections.Generic;
+
+List<string> validCommands = new List<string>();
 
 Console.Write("$ ");
 
-// Wait for user input
+var inputCommand = Console.ReadLine();
+
+if(string.IsNullOrEmpty(inputCommand) || string.IsNullOrEmpty(inputCommand) || !validCommands.Contains(inputCommand)){
+    Console.Write($"{inputCommand}: command not found");
+}
+
 Console.ReadLine();
+
