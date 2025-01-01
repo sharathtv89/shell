27cd6438f05e558256364e550b84a512a2712f0f
sharathtv89 <sharathtv89@mail.com>
2025-01-01 20:37:14 +0530

    Implemented REPL

--- src/main.cs
+++ src/main.cs
@@ -3,14 +3,19 @@ using System.Net.Sockets;
 using System.Collections.Generic;
 
 List<string> validCommands = new List<string>();
+var inputCommand = string.Empty;
+do
+{
+    Console.Write("$ ");
+    inputCommand = Console.ReadLine();
 
-Console.Write("$ ");
+    if(inputCommand == "exit"){
+        return;
+    }
 
-var inputCommand = Console.ReadLine();
-
-if(string.IsNullOrEmpty(inputCommand) || string.IsNullOrEmpty(inputCommand) || !validCommands.Contains(inputCommand)){
-    Console.Write($"{inputCommand}: command not found");
-}
-
-Console.ReadLine();
+    if(string.IsNullOrEmpty(inputCommand) || string.IsNullOrEmpty(inputCommand) || !validCommands.Contains(inputCommand)){
+        Console.WriteLine($"{inputCommand}: command not found");
+    }
+    
+} while (inputCommand != "exit");
 
