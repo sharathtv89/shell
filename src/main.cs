45cc91353c97361598289855e3227affc79bffdc
sharathtv89 <sharathtv89@mail.com>
2025-01-05 11:01:17 +0530

    Implemented echo command

--- src/main.cs
+++ src/main.cs
@@ -1,20 +1,36 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 
-List<string> validCommands = new List<string>();
-var inputCommand = string.Empty;
+List<string> validCommands = ["echo"];
 while(true)
 {
     Console.Write("$ ");
-    inputCommand = Console.ReadLine();
-
-    if(inputCommand == "exit 0"){        
+    var inputText = Console.ReadLine();
+    if(inputText == "exit 0"){        
         return;
     }
+    var inputSlice = inputText?.Split(" ", 2);
+    var command = inputSlice?.Length > 0 ? inputSlice?[0] : " ";
+    var commandParams = inputSlice?.Length == 2 ? inputSlice?[1] : " "; 
 
-    if(string.IsNullOrEmpty(inputCommand) || string.IsNullOrEmpty(inputCommand) || !validCommands.Contains(inputCommand)){
-        Console.WriteLine($"{inputCommand}: command not found");
+  
+
+    if(!validCommands.Contains(command)){
+        Console.WriteLine($"{command}: command not found");
     }
-    
+
+    ProcessCommand(command, commandParams);    
+}
+
+void ProcessCommand(string command, string commandParams)
+{
+    switch(command){
+        case "echo" :
+            Console.WriteLine(commandParams);
+        break;
+        default : break;
+    }
 }
